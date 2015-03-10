b49319062de9c128ba3810501d7f54fcf80deff7
unknown <Perseus.Magbutay@MNL2F2284.nucomm.net>
2015-03-10 12:06:31 +0800

    updated angular script

--- ticketer.js
+++ ticketer.js
@@ -1,10 +1,25 @@
-var ticketer = {};
-
-var tickecore = function()
-{
-	this.init = function(){
-		
-	}
-};
-
-tickecore.call(ticketer);
+angular.module('ticketerApp', [])
+  .controller('ticketController', ['$scope', function($scope) {
+    $scope.tickets = [];
+ 
+    $scope.addTicket = function() {
+      $scope.tickets.push({text:$scope.ticketTitle, done:false});
+      $scope.ticketTitle = '';
+    };
+ 
+    $scope.remaining = function() {
+      var count = 0;
+      angular.forEach($scope.tickets, function(ticket) {
+        count += ticket.done ? 0 : 1;
+      });
+      return count;
+    };
+ 
+    $scope.archive = function() {
+      var oldTickets = $scope.tickets;
+      $scope.tickets = [];
+      angular.forEach(oldTickets, function(ticket) {
+        if (!ticket.done) $scope.tickets.push(ticket);
+      });
+    };
+  }]);
